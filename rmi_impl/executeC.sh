028e73a45079d8a2ddd6de8db246efe795d8b763
Shabir ABDUL SAMADH <sabdul23@ds2.CS.McGill.CA>
2017-10-02 22:23:08 -0400

    Added changes to scripts to execute in remote servers

--- rmi_impl/executeC.sh
+++ rmi_impl/executeC.sh
@@ -3,4 +3,4 @@
 DIR=$(pwd)
 export CLASSPATH=$DIR/clientsrc:$DIR/clientsrc/MiddlewareInterface.jar
 cd clientsrc
-java -Djava.security.policy=java.policy client localhost 1099
+java -Djava.security.policy=java.policy client cs-26.cs.mcgill.ca 1099
--- rmi_impl/executeMid.sh
+++ rmi_impl/executeMid.sh
@@ -6,4 +6,4 @@ DIR=$(pwd)
 export CLASSPATH=$DIR/Middleware:$DIR/Middleware/RMInterface.jar
 cd Middleware
 
-java -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:$DIR/Middleware/ MidImpl.MidWareImpl 1099 localhost localhost localhost
+java -Djava.security.policy=java.policy -Djava.rmi.server.codebase=file:$DIR/Middleware/ MidImpl.MidWareImpl 1099 cs-28.cs.mcgill.ca cs-29.cs.mcgill.ca cs-27.cs.mcgill.ca
