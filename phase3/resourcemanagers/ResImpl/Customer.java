4f41097ba2194ed843dbada1f6506d487f5f1259
Shabir Mean <shabir_tck09@hotmail.com>
2017-11-28 15:06:00 -0500

    brute force copy for customer reservation table

--- phase3/resourcemanagers/ResImpl/Customer.java
+++ phase3/resourcemanagers/ResImpl/Customer.java
@@ -18,7 +18,12 @@ public class Customer extends RMItem {
 
     public Customer(Customer copyCustomer) {
         super();
-        m_Reservations = (RMHashtable) copyCustomer.getReservations().clone();
+//        m_Reservations = (RMHashtable) copyCustomer.getReservations().clone();
+        m_Reservations = new RMHashtable();
+        for (Object key: copyCustomer.getReservations().keySet()) {
+            m_Reservations.put(key, copyCustomer.getReservations().get(key));
+        }
+
         m_nID = copyCustomer.m_nID;
     }
 
