56ae0e9f15f5c8a5c8fc78a17e37fa3afb50a022
Shabir Mean <shabir_tck09@hotmail.com>
2017-12-04 20:41:45 -0500

    reverted remote registry changes

--- phase3/resourcemanagers/ReplicationManager/RMReplicationManager.java
+++ phase3/resourcemanagers/ReplicationManager/RMReplicationManager.java
@@ -104,8 +104,8 @@ public class RMReplicationManager extends ReceiverAdapter implements Serializabl
         try {
             ResourceManager rm = (ResourceManager)
                     UnicastRemoteObject.exportObject((ResourceManagerImpl) resourceManager, 0);
-            Registry registry = LocateRegistry.getRegistry(registryIp, registryPort);
-//            Registry registry = LocateRegistry.getRegistry(registryPort);
+//            Registry registry = LocateRegistry.getRegistry(registryIp, registryPort);
+            Registry registry = LocateRegistry.getRegistry(registryPort);
 //            registry.rebind("ShabirJianResourceManager", rm);
             registry.rebind(registryObjectId, rm);
             System.err.println("RM::Server ready");
